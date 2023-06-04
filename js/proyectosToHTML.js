69b42a92ce3c96567086da32f8ebc5c17dc3e9b0
JonathanB <jonathanbetancorperdomo@gmail.com>
2023-06-04 20:10:26 +0100

    Agregador efecto js-css + proyectos

---
+++ js/proyectosToHTML.js
@@ -0,0 +1,44 @@
+fetch('./js/info_proyectos.json')
+
+  .then(response => response.json())
+  .then(data => {
+    
+    var proyectosContainer = document.querySelector('.proyectos.carrusel .carrusel-contenido');
+    
+    data.proyectos.forEach(function(proyecto) {
+
+        var proyectoElement = document.createElement('div');
+        proyectoElement.classList.add('unProyecto', 'carrusel-item');
+
+        //Contenido
+        var contenidoElement = document.createElement('div');
+        var nombreElement = document.createElement('h3');
+        nombreElement.textContent = proyecto.nombre;
+        var descripcionElement = document.createElement('p');
+        descripcionElement.textContent = proyecto.descripcion;
+
+        //Enlace
+        var enlaceElement = document.createElement('a');
+        enlaceElement.href = proyecto.url;
+        var iconoElement = document.createElement('div');
+        iconoElement.classList.add('icono');
+        var imagenElement = document.createElement('img');
+        imagenElement.src = proyecto.imagen;
+        imagenElement.alt = '';
+
+
+        //Estructura del documento
+        iconoElement.appendChild(imagenElement);
+        enlaceElement.appendChild(iconoElement);
+        contenidoElement.appendChild(nombreElement);
+        contenidoElement.appendChild(descripcionElement);
+        proyectoElement.appendChild(contenidoElement);
+        proyectoElement.appendChild(enlaceElement);
+
+
+        proyectosContainer.appendChild(proyectoElement);
+    });
+  })
+  .catch(error => {
+    console.error('Error al cargar el archivo JSON:', error);
+  });
